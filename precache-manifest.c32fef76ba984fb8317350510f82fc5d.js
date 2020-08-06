db5b85114987ca0b3325dda58e8512252ff05317
James Holding <james.holding90@gmail.com>
2020-08-06 13:20:50 -0400

    Updates

---
+++ precache-manifest.c32fef76ba984fb8317350510f82fc5d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9aa50b5a12fa4535ad53f3dbb9312f44",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "82ae99bbb3e0e22547f4",
+    "url": "/react-portfolio/static/js/2.11283df0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio/static/js/2.11283df0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "820c91e60032bab299f5",
+    "url": "/react-portfolio/static/js/main.96224ff2.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  }
+]);
