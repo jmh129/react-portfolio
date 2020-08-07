c2d3c076ccb4031216d93430b2274eb373dab98c
James Holding <james.holding90@gmail.com>
2020-08-07 11:13:58 -0400

    added button on homepage.

--- src/components/Container/Homepage/Homepage.jsx
+++ src/components/Container/Homepage/Homepage.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import { Link } from "react-scroll";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
+import "../../Navbar/Navbar";
 import "./Homepage.css";
 
 const Homepage = () => {
@@ -10,8 +14,21 @@ const Homepage = () => {
           <br />
           Welcome to my page.
         </h1>
-        <br/>
+        <br />
         <h3>James Holding</h3>
+        <Link
+          className="nav-link"
+          activeClass="active"
+          to="AboutMe"
+          spy={true}
+          smooth={true}
+          offset={-70}
+          duration={500}
+        >
+          <button className="angleDown">
+            <FontAwesomeIcon icon={faChevronDown} />
+          </button>
+        </Link>
       </div>
     </div>
   );
